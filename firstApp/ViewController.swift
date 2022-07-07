e3b027efe7de3831d6171ea089f739541f7d054e
Mike Hong <solitary945@gmail.com>
2022-07-07 19:32:38 +0800

    Initial Commit

---
+++ firstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firstApp
+//
+//  Created by Mike Hong on 2022/07/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
